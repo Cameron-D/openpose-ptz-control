92118dc18e1f516ef7723342f83b36804e798494
Cameron_D <cameron@dewitte.id.au>
2022-02-13 17:41:09 +1000

    Move main program into own class. Still lots of organising to be done

--- pose.py
+++ pose.py
@@ -108,257 +108,242 @@ class ViscaControl(BaseMoveControl):
         self.socket.sendto(message, (args.visca_ip, args.visca_port))
 
 
-control_camera = None
-move_control = None
-video_capture = None
-openpose_wrapper = None
-mqttc = None
-args = None
+class PTZTrack:
+    control_camera = None
 
+    def __init__(self, args):
+        self.args = args
+        self.move = ViscaControl(self.args)
 
-def mqtt_message(client, userdata, message):
-    global control_camera
-    data = message.payload.decode("utf-8")
-    print("Control message:", data)
-    if data.startswith("control state"):
-        mqttc.publish("PTZ_STATE", move_state())
+        # OpenPose Setup
+        params = dict()
+        params["model_folder"] = "/openpose/models"
+        params["net_resolution"] = self.args.net_resolution
 
-    elif data.startswith("control on"):
-        control_camera = True
-        mqttc.publish("PTZ_STATE", move_state())
+        self.openpose = op.WrapperPython()
+        self.openpose.configure(params)
+        self.openpose.start()
 
-    elif data.startswith("control off"):
-        control_camera = False
-        mqttc.publish("PTZ_STATE", move_state())
+        # Video Source Setup
+        self.video_capture = cv2.VideoCapture(args.video_device)
 
-    elif data.startswith("control toggle"):
-        control_camera = not control_camera
-        mqttc.publish("PTZ_STATE", move_state())
-
-
-def calculate_move_speed(smin, val, smax):
-    speed_ratio = (val - smin) / smax
-    speed = int(((args.speed_max - args.speed_min) * speed_ratio) + args.speed_min)
-    return speed
-
-
-def move_state():
-    return "on" if control_camera else "off"
-
-
-def get_keypoints_rectangle(keypoints, threshold=0.2):
-    numberKeypoints = keypoints.shape[0]
-    if numberKeypoints < 1:
-        return "Number body parts must be > 0."
-
-    minX = minY = float("inf")
-    maxX = maxY = float("-inf")
-
-    for keypoint in keypoints:
-        score = keypoint[2]
-        if score > threshold:
-            x = keypoint[0]
-            y = keypoint[1]
-
-            if maxX < x:
-                maxX = x
-            if minX > x:
-                minX = x
-
-            if maxY < y:
-                maxY = y
-            if minY > y:
-                minY = y
-
-    if maxX >= minX and maxY >= minY:
-        return int(minX), int(minY), int(maxX), int(maxY)
-
-
-def sigint_handler(signal_received, frame):
-    global control_camera
-
-    print("Program exit requested... Exiting gracefully")
-    control_camera = False
-    if args.mqtt:
-        mqttc.publish("PTZ_STATE", move_state())
-        time.sleep(0.1)
-        mqttc.loop_stop()
-    video_capture.release()
-    cv2.destroyAllWindows()
-    exit(0)
-
-
-def do_setup():
-    global visca_socket, openpose_wrapper, mqttc, video_capture, move_control
-
-    move_control = ViscaControl(args)
-
-    # OpenPose Setep
-    params = dict()
-    params["model_folder"] = "/openpose/models"
-    params["net_resolution"] = args.net_resolution
-
-    openpose_wrapper = op.WrapperPython()
-    openpose_wrapper.configure(params)
-    openpose_wrapper.start()
-
-    video_capture = cv2.VideoCapture(args.video_device)
-
-    # MQTT Setup
-    if args.mqtt:
-        mqttc = mqtt.Client("PTZTrack")
-        mqttc.connect(args.mqtt_host)
-        mqttc.loop_start()
-        mqttc.subscribe("PTZ_SETSTATE")
-        mqttc.on_message = mqtt_message
-        mqttc.publish("PTZ_STATE", move_state())
-        print("MQTT Connected")
-    else:
-        print("Skipping MQTT Connection")
-
-    signal(SIGINT, sigint_handler)
-
-
-def read_frame():
-    ret, frame = video_capture.read()
-    frame = cv2.resize(frame, (1280, 720))
-    return ret, frame
-
-
-def update_frame_count(fc):
-    fc = +1
-
-    if fc == 50:
-        mqttc.publish("PTZ_STATE", move_state())
-        fc = 0
-
-    return fc
-
-
-def show_ui(frame):
-    if (frame.shape[0] != 720) and (frame.shape[1] != 1280):
-        frame = cv2.resize(frame, (1280, 720))
-
-    cv2.imshow("PTZTrack Frame", frame)
-
-    if cv2.waitKey(25) & 0xFF == ord("q"):
-        return True
-
-
-def calculate_edges(frame_shape):
-    l_edge = int(frame_shape[1] * args.boundary)
-    r_edge = frame_shape[1] - l_edge
-    height = frame_shape[0]
-    width = frame_shape[1]
-
-    bounding = [frame_shape[1], height, 0, 0]
-
-    return l_edge, r_edge, height, width, bounding
-
-
-def process_datum_keypoints(frame, datum):
-    regions = []
-
-    for i in range(0, datum.poseKeypoints.shape[0]):
-        p = get_keypoints_rectangle(datum.poseKeypoints[i], 0.1)
-        regions.append([p[0], p[1], p[2] - p[0], p[3] - p[1]])
-        cv2.rectangle(frame, (p[0], p[1]), (p[2], p[3]), (0, 255, 255), 2)
-
-    return frame, regions
-
-
-def calculate_boundaries(bounding, regions):
-    for (x, y, w, h) in regions:
-        if x < bounding[Edge.LEFT]:
-            bounding[Edge.LEFT] = x
-        if y < bounding[Edge.TOP]:
-            bounding[Edge.TOP] = y
-        if x + w > bounding[Edge.RIGHT]:
-            bounding[Edge.RIGHT] = x + w
-        if y + h > bounding[Edge.BOTTOM]:
-            bounding[Edge.BOTTOM] = y + h
-
-    return bounding
-
-
-def main_loop():
-    bounding = []
-
-    move_control.set_direction(Move.STOP)
-    last_direction = move_control.direction
-
-    move_control.set_speed(args.speed_min)
-    last_speed = move_control.speed
-
-    frame_count = 0
-
-    while video_capture.isOpened():
-        check, frame = read_frame()
-
-        if not check:
-            time.sleep(0.01)
-            continue
-
-        l_edge, r_edge, height, width, bounding = calculate_edges(frame.shape)
-
-        openpose_datum = op.Datum()
-        openpose_datum.cvInputData = frame
-        openpose_wrapper.emplaceAndPop(op.VectorDatum([openpose_datum]))
-        frame = openpose_datum.cvOutputData
-
-        frame, regions = process_datum_keypoints(frame, openpose_datum)
-
-        if len(regions) > 0:
-            bounding = calculate_boundaries(bounding, regions)
-
-            lrmiddle = int(
-                ((bounding[Edge.RIGHT] - bounding[Edge.LEFT]) / 2) + bounding[Edge.LEFT]
-            )
-            udmiddle = int(
-                ((bounding[Edge.BOTTOM] - bounding[Edge.TOP]) / 2) + bounding[Edge.TOP]
-            )
-
-            cv2.rectangle(
-                frame,
-                (bounding[Edge.LEFT], bounding[Edge.TOP]),
-                (bounding[Edge.RIGHT], bounding[Edge.BOTTOM]),
-                (0, 200, 0),
-                2,
-            )
-            cv2.rectangle(
-                frame,
-                (lrmiddle - 1, udmiddle - 1),
-                (lrmiddle + 1, udmiddle + 1),
-                (255, 255, 0),
-                4,
-            )
-            cv2.rectangle(frame, (l_edge, 0), (r_edge, height), (255, 0, 0), 4)
-
-            if lrmiddle < l_edge:
-                speed = calculate_move_speed(0, l_edge - lrmiddle, l_edge)
-                move_control.set_direction(Move.LEFT)
-            elif lrmiddle > r_edge:
-                speed = calculate_move_speed(r_edge, lrmiddle, width)
-                move_control.set_direction(Move.RIGHT)
-            else:
-                move_control.set_speed(args.speed_min)
-                move_control.set_direction(Move.STOP)
+        # MQTT Setup
+        if self.args.mqtt:
+            self.mqttc = mqtt.Client("PTZTrack")
+            self.mqttc.connect(args.mqtt_host)
+            self.mqttc.loop_start()
+            self.mqttc.subscribe("PTZ_SETSTATE")
+            self.mqttc.on_message = self.mqtt_message
+            self.mqtt_publish_state()
+            print("MQTT Connected")
         else:
-            move_control.set_direction(Move.STOP)
+            print("Skipping MQTT Connection")
 
-        if move_control.direction != last_direction or move_control.speed != last_speed:
-            if control_camera:
-                move_control.do_move()
-            last_direction = move_control.direction
-            last_speed = move_control.speed
+        signal(SIGINT, self.sigint_handler)
 
-        # Showing the output Image
-        if args.ui:
-            if show_ui(frame):
-                break
+    def mqtt_message(self, client, userdata, message):
+        data = message.payload.decode("utf-8")
+        print("Control message:", data)
 
-        if args.mqtt:
-            frame_count = update_frame_count(frame_count)
+        if data.startswith("control state"):
+            self.mqtt_publish_state()
+
+        elif data.startswith("control on"):
+            self.control_camera = True
+            self.mqtt_publish_state()
+
+        elif data.startswith("control off"):
+            self.control_camera = False
+            self.mqtt_publish_state()
+
+        elif data.startswith("control toggle"):
+            self.control_camera = not self.control_camera
+            self.mqtt_publish_state()
+
+    def calculate_speed(self, smin: int, val: int, smax: int):
+        speed_ratio = (val - smin) / smax
+        speed = int(
+            ((self.args.speed_max - self.args.speed_min) * speed_ratio)
+            + self.args.speed_min
+        )
+        return speed
+
+    def move_state_str(self):
+        return "on" if self.control_camera else "off"
+
+    def get_keypoints_rectangle(self, keypoints, threshold=0.2):
+        numberKeypoints = keypoints.shape[0]
+        if numberKeypoints < 1:
+            return "Number body parts must be > 0."
+
+        minX = minY = float("inf")
+        maxX = maxY = float("-inf")
+
+        for keypoint in keypoints:
+            score = keypoint[2]
+            if score > threshold:
+                x = keypoint[0]
+                y = keypoint[1]
+
+                if maxX < x:
+                    maxX = x
+                if minX > x:
+                    minX = x
+
+                if maxY < y:
+                    maxY = y
+                if minY > y:
+                    minY = y
+
+        if maxX >= minX and maxY >= minY:
+            return int(minX), int(minY), int(maxX), int(maxY)
+
+    def sigint_handler(self, signal_received, frame):
+        print("Program exit requested... Exiting gracefully")
+        self.control_camera = False
+        if self.args.mqtt:
+            self.mqtt_publish_state()
+            time.sleep(0.1)
+            self.mqttc.loop_stop()
+        self.video_capture.release()
+        cv2.destroyAllWindows()
+        exit(0)
+
+    def read_frame(self):
+        ret, frame = self.video_capture.read()
+        frame = cv2.resize(frame, (1280, 720))
+        return ret, frame
+
+    def mqtt_publish_state(self):
+        self.mqttc.publish("PTZ_STATE", self.move_state_str())
+
+    def show_ui(self, frame):
+        if (frame.shape[0] != 720) and (frame.shape[1] != 1280):
+            frame = cv2.resize(frame, (1280, 720))
+
+        cv2.imshow("PTZTrack Frame", frame)
+
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            return True
+
+    def calculate_edges(self, frame_shape):
+        l_edge = int(frame_shape[1] * args.boundary)
+        r_edge = frame_shape[1] - l_edge
+        height = frame_shape[0]
+        width = frame_shape[1]
+
+        bounding = [frame_shape[1], height, 0, 0]
+
+        return l_edge, r_edge, height, width, bounding
+
+    def process_datum_keypoints(self, frame, datum):
+        regions = []
+
+        for i in range(0, datum.poseKeypoints.shape[0]):
+            p = self.get_keypoints_rectangle(datum.poseKeypoints[i], 0.1)
+            regions.append([p[0], p[1], p[2] - p[0], p[3] - p[1]])
+            cv2.rectangle(frame, (p[0], p[1]), (p[2], p[3]), (0, 255, 255), 2)
+
+        return frame, regions
+
+    def calculate_boundaries(self, bounding, regions):
+        for (x, y, w, h) in regions:
+            if x < bounding[Edge.LEFT]:
+                bounding[Edge.LEFT] = x
+            if y < bounding[Edge.TOP]:
+                bounding[Edge.TOP] = y
+            if x + w > bounding[Edge.RIGHT]:
+                bounding[Edge.RIGHT] = x + w
+            if y + h > bounding[Edge.BOTTOM]:
+                bounding[Edge.BOTTOM] = y + h
+
+        return bounding
+
+    def main_loop(self):
+        bounding = []
+
+        self.move.set_direction(Move.STOP)
+        last_direction = self.move.direction
+
+        self.move.set_speed(self.args.speed_min)
+        last_speed = self.move.speed
+
+        frame_count = 0
+
+        while self.video_capture.isOpened():
+            check, frame = self.read_frame()
+
+            if not check:
+                time.sleep(0.01)
+                continue
+
+            l_edge, r_edge, height, width, bounding = self.calculate_edges(frame.shape)
+
+            openpose_datum = op.Datum()
+            openpose_datum.cvInputData = frame
+            self.openpose.emplaceAndPop(op.VectorDatum([openpose_datum]))
+            frame = openpose_datum.cvOutputData
+
+            frame, regions = self.process_datum_keypoints(frame, openpose_datum)
+
+            if len(regions) > 0:
+                bounding = self.calculate_boundaries(bounding, regions)
+
+                lrmiddle = int(
+                    ((bounding[Edge.RIGHT] - bounding[Edge.LEFT]) / 2)
+                    + bounding[Edge.LEFT]
+                )
+                udmiddle = int(
+                    ((bounding[Edge.BOTTOM] - bounding[Edge.TOP]) / 2)
+                    + bounding[Edge.TOP]
+                )
+
+                cv2.rectangle(
+                    frame,
+                    (bounding[Edge.LEFT], bounding[Edge.TOP]),
+                    (bounding[Edge.RIGHT], bounding[Edge.BOTTOM]),
+                    (0, 200, 0),
+                    2,
+                )
+                cv2.rectangle(
+                    frame,
+                    (lrmiddle - 1, udmiddle - 1),
+                    (lrmiddle + 1, udmiddle + 1),
+                    (255, 255, 0),
+                    4,
+                )
+                cv2.rectangle(frame, (l_edge, 0), (r_edge, height), (255, 0, 0), 4)
+
+                if lrmiddle < l_edge:
+                    self.move.set_speed(
+                        self.calculate_speed(0, l_edge - lrmiddle, l_edge)
+                    )
+                    self.move.set_direction(Move.LEFT)
+                elif lrmiddle > r_edge:
+                    self.move.set_speed(self.calculate_speed(r_edge, lrmiddle, width))
+                    self.move.set_direction(Move.RIGHT)
+                else:
+                    self.move.set_speed(args.speed_min)
+                    self.move.set_direction(Move.STOP)
+            else:
+                self.move.set_direction(Move.STOP)
+
+            if self.move.direction != last_direction or self.move.speed != last_speed:
+                if self.control_camera:
+                    self.move.do_move()
+                last_direction = self.move.direction
+                last_speed = self.move.speed
+
+            # Showing the output Image
+            if self.args.ui:
+                if self.show_ui(frame):
+                    break
+
+            # Publish control state occasionally
+            frame_count += 1
+            if self.args.mqtt and frame_count == 20:
+                self.mqtt_publish_state()
+                frame_count = 0
 
 
 if __name__ == "__main__":
@@ -432,11 +417,12 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    if args.mqtt:
-        control_camera = args.control
-    else:
-        control_camera = True
+    ptztrack = PTZTrack(args)
 
-    do_setup()
-    main_loop()
-    sigint_handler(None, None)  # force tidy exit
+    if args.mqtt:
+        ptztrack.control_camera = args.control
+    else:
+        ptztrack.control_camera = True
+
+    ptztrack.main_loop()
+    ptztrack.sigint_handler(None, None)  # force tidy exit
