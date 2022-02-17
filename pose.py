b6081a49c51315838e7963a7a0bcb051a1d4835f
Cameron_D <cameron@dewitte.id.au>
2022-02-17 20:09:44 +1000

    Split up movement classes into seperate module

--- pose.py
+++ pose.py
@@ -1,9 +1,10 @@
-import cv2, socket, time, socket, argparse, time
+import cv2, time, socket, argparse, time
 import pyopenpose as op
 import paho.mqtt.client as mqtt
 from enum import IntEnum
 from signal import signal, SIGINT
 from sys import exit
+from move import ViscaControl
 
 
 class Move(IntEnum):
@@ -17,91 +18,6 @@ class Edge(IntEnum):
     TOP = 1
     RIGHT = 2
     BOTTOM = 3
-
-
-class BaseMoveControl:
-    direction = None
-    speed = None
-
-    def __init__(self, args):
-        raise NotImplementedError
-
-    def do_move(self):
-        raise NotImplementedError
-
-    def set_direction(self, direction: Move):
-        raise NotImplementedError
-
-    def set_speed(self, speed: int):
-        raise NotImplementedError
-
-
-class ViscaControl(BaseMoveControl):
-    SEQUENCE_RESET = "02 00 00 01 00 00 00 01 01"
-    MOVE_HEADER = "81 01 06 01 "
-    LEFT = " 01 03 FF"
-    RIGHT = " 02 03 FF"
-    STOP = " 03 03 FF"
-
-    def __init__(self, args):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.host = args.visca_ip
-        self.port = args.visca_port
-
-        self.sequence_number = 1
-
-        self.set_direction(Move.STOP)
-        self.set_speed(args.speed_min)
-
-    def set_direction(self, direction: Move):
-        self.direction = direction
-
-    def set_speed(self, speed: int):
-        self.speed = speed
-
-    def do_move(self):
-        self.reset_sequence_number()
-
-        if self.direction == Move.LEFT:
-            self.send_packet(self.make_move_str(self.LEFT))
-        elif self.direction == Move.RIGHT:
-            self.send_packet(self.make_move_str(self.RIGHT))
-        else:
-            self.send_packet(self.make_move_str(self.STOP))
-
-        print(
-            "MOVE:",
-            Move(self.direction),
-            "@ speed",
-            self.speed,
-            "({0:0{1}x})".format(int(self.speed), 2),
-        )
-
-    def reset_sequence_number(self):
-        self.socket.sendto(
-            bytearray.fromhex(self.SEQUENCE_RESET), (self.host, self.port)
-        )
-        self.sequence_number = 1
-
-    def make_move_str(self, direction_str: str):
-        # returns a padded hex value without 0x
-        spd_hex = "{0:0{1}x}".format(int(self.speed), 2)
-        return self.MOVE_HEADER + spd_hex + spd_hex + direction_str
-
-    def send_packet(self, command: str):
-        payload_type = bytearray.fromhex("01 00")
-        payload = bytearray.fromhex(command)
-        payload_length = len(payload).to_bytes(2, "big")
-
-        message = (
-            payload_type
-            + payload_length
-            + self.sequence_number.to_bytes(4, "big")
-            + payload
-        )
-
-        self.sequence_number += 1
-        self.socket.sendto(message, (args.visca_ip, args.visca_port))
 
 
 class PTZTrack:
